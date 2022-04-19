de1f9c5ac0d595ea20f1c801430d213b358e632c
alex <48461058+alexramos17@users.noreply.github.com>
2022-04-19 14:56:59 -0400

    creating first bot

--- ping.py
+++ ping.py
@@ -1,3 +1,16 @@
-import discord.py
+import os
+import discord
+from dotenv import load_dotenv
 
-
+load_dotenv()
+TOKEN = os.getenv(
+    'e555f982ef876529e12c97790dfd11daff5d3ad871cd939226e44363cb46b7ca')
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+client.run(TOKEN)
