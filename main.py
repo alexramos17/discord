a6f3371a032194a6255df75c09b71237da3b1020
alex <48461058+alexramos17@users.noreply.github.com>
2022-04-19 17:30:03 -0400

    adding a command for hi

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
+from cgitb import reset
 import os
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 
 client = discord.Client()
 
+bot = commands.Bot(command_prefix='!')
+
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
+
+@bot.commands(name='hello', help='Responds with Hi!')
+async def hello(ctx):
+    # if message.author == client.author:
+    #    return
+
+    response = 'Hi!'
+    await ctx.send(response)
 
 client.run(os.getenv('TOKEN'))
