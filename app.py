b301ee354923ab3bc3df5f057a39ed5beb93814b
Alex <48461058+level-project@users.noreply.github.com>
2023-02-19 01:01:31 -0500

    I'm dumb and was using the wrong package (yt-dlp)

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 from discord.utils import get
 from discord import FFmpegPCMAudio
 from dotenv import load_dotenv
-from youtube_dl import YoutubeDL
+from yt_dlp import YoutubeDL
 
 load_dotenv()
 
